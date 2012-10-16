e26866411e8958aadd01bb0dfa880fcb14732a6c
fedor <>
2012-10-16 18:23:57 +0000

    Statistic stuff for SUS-12-022

---
+++ Statistics/2012/scans/TChiSlepSnu_2i_0.05/SS/runLimitLands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export PATH=`pwd`/../../../bin:`pwd`/../../../bin/LandS/test:$PATH
+echo $PATH
+maxsignal=75
+channels=10
+basenm=SUSY_scan_v1
+
+if [ -z "$SGE_TASK_ID" ] 
+   then 
+   echo runRa7LimitLands $1 $maxsignal $channels
+   runRa7LimitLands $1 $maxsignal $channels
+else 
+   echo runRa7LimitLands $basenm.$SGE_TASK_ID.txt $maxsignal $channels
+   runRa7LimitLands $basenm.$SGE_TASK_ID.txt $maxsignal $channels
+fi
